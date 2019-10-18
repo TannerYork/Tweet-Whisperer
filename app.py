8a15a8fad6263430e5694e4bc5f9b2bf4d7c68b0
Tanner York <tannerwyork@gmail.com>
2019-10-18 11:28:00 -0700

    Add ability to search topics and get tweets

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
 import tweepy
 from tensorflow import keras
@@ -12,16 +12,22 @@ ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 app = Flask(__name__)
 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
 
 @app.route('/')
 def index():
     '''Render the landing page for the website'''
-    return render_template('index.html', topic=None)
+    return render_template('index.html', tweets=[])
 
 @app.route('/topic', methods=['POST'])
 def search_tweets():
     '''Get tweets from twitter search api and process sentiment'''
-    return redirect(url_for('index'))
+    query = request.form.get('query')
+    tweets = [tweet.text for tweet in api.search(q=query, lang='en', rpp=10)]
+    return render_template('index.html', tweets=tweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
